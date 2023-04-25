d17701a27a05eba5cfa15362bf12d5f5cb5b7962
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-25 04:02:19 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,25 +7,29 @@ import sys
 
 if __name__ == "__main__":
     idee = sys.argv[1]
-    NAME = ""
+    USERNAME = ""
     ENTRY = []
     file_path = idee + ".csv"
 
     # Retrieving the employee name
     api_url = "https://jsonplaceholder.typicode.com/users/" + idee
     response = requests.get(api_url)
-    NAME = response.json().get("name")
+    USERNAME = response.json().get("username")
 
     # Retrieving the employee completed tasks
     api_url = "https://jsonplaceholder.typicode.com/users/" + idee + "/todos"
     response = requests.get(api_url)
 
     with open(file_path, 'w') as f:
-        f_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        w = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        # f_writer.delimiter = ','
+        # f_writer.quotechar = '"'
+        # f_writer.quoting = csv.QUOTE_ALL
         for todo in response.json():
             ENTRY.append(todo.get("userId"))
-            ENTRY.append(NAME)
+            ENTRY.append(USERNAME)
             ENTRY.append(todo.get("completed"))
             ENTRY.append(todo.get("title"))
-            f_writer.writerow(ENTRY)
+            w.writerow(ENTRY)
+            ENTRY = []
     f.close()
