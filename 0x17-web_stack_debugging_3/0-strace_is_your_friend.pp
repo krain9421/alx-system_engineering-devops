d29a30e4b0266378bdff6ad616908dbfb835eecc
Nwigwe Chidubem <krain9421@gmail.com>
2023-08-10 05:07:46 +0000

    Modified puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,7 @@
 # Find out why Apache is returning a 500 error.
 
 exec { 'sed':
-	path	=> 'usr/bin:/bin'
+  path    => 'usr/bin:/bin',
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.pp',
+  returns => [0,1],
+}
