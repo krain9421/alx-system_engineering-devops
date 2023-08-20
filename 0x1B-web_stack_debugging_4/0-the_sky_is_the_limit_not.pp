d58c5f8627d83c388610249e7f67354157583a8c
Nwigwe Chidubem <krain9421@gmail.com>
2023-08-20 09:06:57 +0000

    commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,6 +6,12 @@ exec { 'sed':
   returns => [0,1],
 }
 
+exec { 'sed2':
+  path    => 'usr/bin:/bin',
+  command => 'sed -i "s/pid \/run\/nginx.pid;/pid \/run\/nginx.pid;\tworker_rlimit_nofile 4096;/g" /etc/nginx/nginx.conf',
+  returns => [0,1],
+}
+
 exec { 'service':
   path    => 'usr/bin:/bin',
   command => 'service nginx restart',
