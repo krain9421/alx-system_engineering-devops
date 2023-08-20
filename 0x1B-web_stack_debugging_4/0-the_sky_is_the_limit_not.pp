cc2ebdfe45a2cde1a0e3af26e0807c0b74b11f65
Nwigwe Chidubem <krain9421@gmail.com>
2023-08-20 08:46:13 +0000

    commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Find out why Nginx is getting many failed requests
+
+exec { 'sed':
+  path    => 'usr/bin:/bin',
+  command => 'sed -i "s/ULIMIT=/"-n 15/"/ULIMIT=/"-n 4096/"/g" /etc/default/nginx',
+  returns => [0,1],
+}
+
+exec { 'service':
+  path    => 'usr/bin:/bin',
+  command => 'service nginx restart',
+  returns => [0,1],
+}
