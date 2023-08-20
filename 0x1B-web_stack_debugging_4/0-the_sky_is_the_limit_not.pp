3a52f603dc2f7dea9230746805c0787ec119e212
Nwigwe Chidubem <krain9421@gmail.com>
2023-08-20 09:53:10 +0000

    fixed number of blocks in puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,7 @@
 # Find out why Nginx is getting many failed requests
 
-exec { 'sed':
+exec { 'fix--for-nginx':
   path    => 'usr/bin:/bin',
   command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 30000\"/g" /etc/default/nginx && service nginx restart',
+  returns => 0,
 }
-#exec { 'sed2':
-#  path    => 'usr/bin:/bin',
-#  command => 'sed -i "s/pid \/run\/nginx.pid;/pid \/run\/nginx.pid;\nworker_rlimit_nofile 4096;/g" /etc/nginx/nginx.conf',
-#  returns => [0,1],
-#}
-
-#exec { 'service':
-#  path    => 'usr/bin:/bin',
-#  command => 'service nginx restart',
-#  returns => [0,1],
-#}
